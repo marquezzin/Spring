21448bed9429f4f0b42f020fb01ba2a2a77039ba
marquezin <gabrielmarques2003@gmail.comgit config --global user.email gabrielmarques2003@gmail.comgit config --global user.name marquezzinclear>
2024-07-29 19:05:36 -0300

    update

--- src/main/java/br/com/marquezzin/todolist/task/TaskController.java
+++ src/main/java/br/com/marquezzin/todolist/task/TaskController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.marquezzin.todolist.utils.Utils;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,7 +28,6 @@ public class TaskController {
 
     @PostMapping("/")
     public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
-        System.out.println("Chegou na controller ");
         var idUser = request.getAttribute("idUser");
         taskModel.setIdUser((UUID) idUser);
 
@@ -42,7 +42,7 @@ public class TaskController {
                     .body("A data de início deve ser menor que a data de término");
 
         }
-        var task = this.taskRepository.save(taskModel); // método da interface
+        var task = this.taskRepository.save(taskModel); // método da interface, salva a task criada
         return ResponseEntity.status(HttpStatus.OK).body(task);
 
     }
@@ -57,11 +57,14 @@ public class TaskController {
 
     // http://localhost:8080/tasks/313132
     @PutMapping("/{id}")
-    public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
-        var idUser = request.getAttribute("idUser");
-        taskModel.setIdUser((UUID) idUser);
-        taskModel.setId(id);
-        return this.taskRepository.save(taskModel);
+    public TaskModel update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {
+
+        var task = this.taskRepository.findById(id).orElse(null);
+        // nao nulo do source é o que foi alterado
+        // e ele vai pro target
+        Utils.copyNonNullProperties(taskModel, task);
+
+        return this.taskRepository.save(task);
 
     }
 
--- src/main/java/br/com/marquezzin/todolist/utils/Utils.java
+++ src/main/java/br/com/marquezzin/todolist/utils/Utils.java
@@ -4,15 +4,47 @@ import java.beans.PropertyDescriptor;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.BeanWrapper;
 import org.springframework.beans.BeanWrapperImpl;
 
 public class Utils {
-    public String[] getNullPropertyNames(Object source) {
+    // copia propriedades não nulas de um objeto source para um objeto target
+    public static void copyNonNullProperties(Object source, Object target) {
+        // getnull..: Retorna um array de nomes de propriedades que são nulas no objeto
+        // source,sendo ignoradas na cópia.
+        BeanUtils.copyProperties(source, target, getNullPropertyNames(source));
+    }
+
+    // pegar propriedades nulas
+    public static String[] getNullPropertyNames(Object source) {
+        // BeanWrapper é uma interface do java que permite acessar as propriedades de um
+        // obj
+        // WrapperImpl é a implementação dessa interface
         final BeanWrapper src = new BeanWrapperImpl(source);
 
+        // Array com as propriedades do objeto
         PropertyDescriptor[] pds = src.getPropertyDescriptors();
 
+        // Criação de um conjunto vazio de Strings
+        // é utilizada para criar uma coleção de elementos do tipo String que não
+        // permite duplicatas.
         Set<String> emptyNames = new HashSet<>();
+
+        // iteração for each do tipo:
+        // for (TipoElemento elemento : coleçãoOuArray) {
+        // bloco de código a ser executado para cada elemento
+        // }
+        for (PropertyDescriptor pd : pds) {
+            // valor da propriedade atual
+            Object srcValue = src.getPropertyValue(pd.getName());
+            if (srcValue == null) {
+                emptyNames.add(pd.getName());
+            }
+        }
+        // array de strings (String[]) com um tamanho baseado no número de elementos em
+        // emptyNames
+        String[] result = new String[emptyNames.size()];
+        return emptyNames.toArray(result);
     }
 }
